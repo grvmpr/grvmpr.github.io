94a0acc9fed039c64b21de08ba622a1697cadfdc
Grumper <grvmpr@pm.me>
2022-07-22 18:30:53 +1000

    Update PoEMapTiers.js

--- PoEMapTiers.js
+++ PoEMapTiers.js
@@ -65,11 +65,11 @@ function apiGetMapData(callback) {
 
 	const req = new XMLHttpRequest();
 	req.open("GET", url, true);
-	req.setRequestHeader("OData-MaxVersion", "4.0");
-	req.setRequestHeader("OData-Version", "4.0");
+	//req.setRequestHeader("OData-MaxVersion", "4.0");
+	//req.setRequestHeader("OData-Version", "4.0");
 	req.setRequestHeader("Accept", "application/json");
 	req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-	req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+	//req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
 	req.onreadystatechange = function () {
 		if (this.readyState === 4) {
 			req.onreadystatechange = null;
